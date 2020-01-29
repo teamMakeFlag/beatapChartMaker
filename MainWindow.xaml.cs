fbf5a44c0940137cb9adead4eb514322fdf7a548
minfaox3 <spdlci30@gmail.com>
2020-01-29 17:48:15 +0900

    [add]新しいプロジェクト,　プロジェクトを開くなど

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Forms = System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace BeatapChartMaker
 {
@@ -20,9 +24,225 @@ namespace BeatapChartMaker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static String WSP = "WorkSpace\\";
+        private string[] ApprovalExt = { ".csv", ".txt" };
+        private string[] ApprovalAudioExt = { ".mp3", ".ogg", ".wav", ".aif" };
+        private string[] ApprovalImageExt = { ".psd", ".tga", ".tiff", ".png", ".pict", ".jpg", ".jpeg", ".iff", ".hdr", ".gif", ".exr", ".bmp" };
+        private String ProjectDirectoryPath = "";
+        private String SongName = "";
+        private String ArtistName = "";
+        private String AudioFilePath = "";
+        private String ThumbFilePath = "";
+        private String ChartPath = "";
+        private String ChartName = "";
+        private String ChartJudge = "";
+        private String ChartDesignerName = "";
+        private int ChartStandardBPM = 0;
+        private int ChartLevel = 0;
+        private int ChartOffset = 0;
+        private List<String> ChartPaths = new List<String>();
+        private List<String> ChartNames = new List<String>();
+        private List<String> ChartJudges = new List<String>();
+        private List<String> ChartDesigners = new List<String>();
+        private List<int> ChartStandardBPMs = new List<int>();
+        private List<int> ChartLevels = new List<int>();
+        private List<int> ChartOffsets = new List<int>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NewProjectButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            NewProject np = new NewProject();
+            np.Show();
+        }
+        private void OpenProjectButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            Forms.FolderBrowserDialog fbd = new Forms.FolderBrowserDialog();
+            fbd.Description = "作業(プロジェクト)フォルダを選択してください";
+            if (fbd.ShowDialog() == Forms.DialogResult.OK)
+            {
+                ProjectDirectoryPath = fbd.SelectedPath;
+                OpenProject(ProjectDirectoryPath);
+            }
+        }
+
+        private void ThumbRefButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "画像ファイル|*.psd;*.tga;*.tiff;*.png;*.pict;*.jpg;*.jpeg;*.iff;*.hdr;*.gif;*.exr;*.bmp";
+            if (fd.ShowDialog() == true) { ThumbFilePath = fd.FileName; ThumbFilePathTBox.Text = fd.FileName; }
+        }
+        private void AudioRefButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "楽曲ファイル|*.mp3;*.ogg;*.wav;*.aif";
+            if (fd.ShowDialog() == true) { AudioFilePath = fd.FileName; AudioFilePathTBox.Text = fd.FileName; }
+        }
+
+        string CheckExt(string str)
+        {
+            foreach (string ext in ApprovalExt)
+            {
+                if (str.Length - ext.Length == str.LastIndexOf(ext)) return ext;
+            }
+            return "";
+        }
+        Boolean CheckExt(string[] exts, string str)
+        {
+            foreach (string ext in exts)
+            {
+                if (str.Length - ext.Length == str.LastIndexOf(ext)) return true;
+            }
+            return false;
+        }
+        public void OpenProject(String ProjectPath)
+        {
+            Boolean isMusicTxtRead = false;
+            Boolean isAudioFileRead = false;
+            Boolean isThumbFileRead = false;
+            DirectoryInfo directory = new DirectoryInfo(ProjectPath);
+            foreach (FileInfo inf in directory.GetFiles())
+            {
+                string infstr = inf + "";
+                if (CheckExt(infstr) == ".txt")
+                {
+                    if (!isMusicTxtRead)
+                    {
+                        StreamReader sr = new StreamReader(@ProjectPath + "\\" + infstr, System.Text.Encoding.Default);
+                        string value = sr.ReadToEnd();
+                        sr.Close();
+                        string[] values = value.Split('\n');
+                        SongName = values[0];
+                        ArtistName = values[1];
+                        isMusicTxtRead = true;
+                    }
+                    else if (isAudioFileRead)
+                    {
+                        Forms.MessageBox.Show("曲情報が記載されたテキストファイルが2個以上あります!\n修正してください!", "プロジェクト読み込みエラー", Forms.MessageBoxButtons.OK, Forms.MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+                else if (CheckExt(ApprovalAudioExt, infstr))
+                {
+                    if (!isAudioFileRead)
+                    {
+                        AudioFilePath = infstr;
+                        isAudioFileRead = true;
+                    }
+                    else if (isAudioFileRead)
+                    {
+                        Forms.MessageBox.Show("楽曲ファイルが2個以上あります!\n修正してください!", "プロジェクト読み込みエラー", Forms.MessageBoxButtons.OK, Forms.MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+                else if (CheckExt(infstr) == ".csv")
+                {
+                    ChartPaths.Add(infstr);
+                    StreamReader sr = new StreamReader(@ProjectPath + "\\" + infstr, System.Text.Encoding.Default);
+                    string value = sr.ReadToEnd();
+                    sr.Close();
+                    string[] values = value.Split(',');
+                    if (values.Length < 6)
+                    {
+                        Forms.MessageBox.Show("譜面ファイルに不正なフォーマットが使われています!\n修正または削除してください!\nError occurred in " + infstr + " file.", "プロジェクト読み込みエラー", Forms.MessageBoxButtons.OK, Forms.MessageBoxIcon.Error);
+                        return;
+                    }
+                    ChartPaths.Add(infstr);
+                    ChartNames.Add(values[0]);
+                    ChartLevels.Add(int.Parse(values[1]));
+                    ChartDesigners.Add(values[2]);
+                    ChartStandardBPMs.Add(int.Parse(values[3]));
+                    ChartOffsets.Add(int.Parse(values[4]));
+                    ChartJudges.Add(values[5]);
+                }
+                else if (CheckExt(ApprovalImageExt, infstr))
+                {
+                    if (!isThumbFileRead)
+                    {
+                        ThumbFilePath = infstr;
+                        isThumbFileRead = true;
+                    }
+                    else if (isThumbFileRead)
+                    {
+                        Forms.MessageBox.Show("画像ファイルが2個以上あります!\n修正してください!", "プロジェクト読み込みエラー", Forms.MessageBoxButtons.OK, Forms.MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+            }
+            SongNameTBox.Text = SongName;
+            ArtistNameTBox.Text = ArtistName;
+            AudioFilePathTBox.Text = AudioFilePath;
+            ThumbFilePathTBox.Text = ThumbFilePath;
+        }
+
+        private void ChartLevelTBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !new Regex("[0-9,]").IsMatch(e.Text);
+        }
+
+        private void ChartOffsetTBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !new Regex("[0-9]").IsMatch(e.Text);
+        }
+
+        private void ChartStandardBPMTBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !new Regex("[0-9]").IsMatch(e.Text);
+        }
+
+        private void SongNameTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            SongName = SongNameTBox.Text.ToString();
+        }
+
+        private void ArtistNameTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ArtistName = ArtistNameTBox.Text.ToString();
+        }
+
+        private void ThumbFilePathTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            if (!File.Exists(ThumbFilePathTBox.Text.ToString())) ThumbFilePath = "";
+            else ThumbFilePath = ThumbFilePathTBox.Text.ToString();
+        }
+
+        private void AudioFilePathTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            if (!File.Exists(AudioFilePathTBox.Text.ToString())) AudioFilePath = "";
+            else AudioFilePath = AudioFilePathTBox.Text.ToString();
+        }
+
+        private void ChartDesignerNameTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ChartDesignerName = ChartDesignerNameTBox.Text.ToString();
+        }
+
+        private void ChartNameTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ChartName = ChartNameTBox.Text.ToString();
+        }
+
+        private void ChartJudgeTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ChartJudge = ChartJudgeTBox.Text.ToString();
+        }
+
+        private void ChartLevelTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ChartLevel = Convert.ToInt32(ChartLevelTBox.Text.ToString());
+        }
+
+        private void ChartOffsetTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ChartOffset = Convert.ToInt32(ChartOffsetTBox.Text.ToString());
+        }
+
+        private void ChartStandardBPMTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ChartStandardBPM = Convert.ToInt32(ChartStandardBPMTBox.Text.ToString());
         }
     }
 }
---
+++ NewProject.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace BeatapChartMaker
+{
+    /// <summary>
+    /// NewProject.xaml の相互作用ロジック
+    /// </summary>
+    public partial class NewProject : Window
+    {
+        private String WorkSpaceDirectoryName = "";
+        private String SongName = "";
+        private String ArtistName = "";
+        private String ThumbFilePath = "";
+        private String AudioFilePath = "";
+        private String ChartDesignerName = "";
+        private String ChartDifficulities = "";
+        private String ChartJudges = "";
+        private String ChartLevels = "";
+        private String StandardBPM = "";
+        private static String WSP = "WorkSpace\\";
+        public NewProject()
+        {
+            InitializeComponent();
+            this.Activate();
+            this.Topmost = true;
+        }
+
+        private Boolean isStringLegitimateAsWFName(String name)
+        {
+            if (name.IndexOf('\\') == -1) return false;
+            if (name.IndexOf('/') == -1) return false;
+            if (name.IndexOf(':') == -1) return false;
+            if (name.IndexOf('*') == -1) return false;
+            if (name.IndexOf('?') == -1) return false;
+            if (name.IndexOf('"') == -1) return false;
+            if (name.IndexOf('<') == -1) return false;
+            if (name.IndexOf('>') == -1) return false;
+            if (name.IndexOf('|') == -1) return false;
+            return true;
+        }
+
+        private void WorkSpaceDirectoryNameTBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !new Regex("[^/:*?\"<>|]").IsMatch(e.Text);
+        }
+
+        private void ChartLevelsTBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !new Regex("[0-9,]").IsMatch(e.Text);
+        }
+
+        private void StandardBPMTBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !new Regex("[0-9]").IsMatch(e.Text);
+        }
+
+        private void CreateButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            string[] difs = ChartDifficulities.Split(',');
+            string[] juds = ChartJudges.Split(',');
+            string[] levs = ChartLevels.Split(',');
+            if (difs.Length == juds.Length && levs.Length == difs.Length && WorkSpaceDirectoryName != "" && SongName != "" && ArtistName != "" && ThumbFilePath !="" && AudioFilePath != "" && ChartDesignerName != "" && ChartDifficulities != "" && ChartLevels != "" && StandardBPM != "")
+            {
+                Directory.CreateDirectory(WSP + WorkSpaceDirectoryName);
+                StreamWriter fs = new StreamWriter(@WSP + WorkSpaceDirectoryName + "\\music.txt", false, System.Text.Encoding.Default);
+                fs.Write(SongName + "\n");
+                fs.Write(ArtistName);
+                fs.Close();
+                if(ThumbFilePath!="None")File.Copy(@ThumbFilePath, @WSP + WorkSpaceDirectoryName + "\\thumb" + ThumbFilePath.Substring(ThumbFilePath.LastIndexOf(".")));
+                File.Copy(@AudioFilePath, @WSP + WorkSpaceDirectoryName + "\\music" + AudioFilePath.Substring(AudioFilePath.LastIndexOf(".")));
+                for (int j = 0; j < difs.Length; j++)
+                {
+                    String path = WSP + WorkSpaceDirectoryName + "\\" + difs[j] + ".csv";
+                    StreamWriter cfs = new StreamWriter(@path, false, System.Text.Encoding.Default);
+                    cfs.Write(difs[j] + "," + levs[j] + "," + ChartDesignerName + "," + StandardBPM + ",0," + juds[j] + "\n");
+                    cfs.Write("START,,,,,\n");
+                    cfs.Write("END,,,,,\n");
+                    cfs.Close();
+                }
+                //←親ウィンドウのOpenProjectを呼ぶ
+                this.Close();
+            }
+            else MessageBox.Show("不正な値です.すべての欄に記入したか確かめてください.");
+        }
+
+        private void ThumbRefButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "画像ファイル|*.psd;*.tga;*.tiff;*.png;*.pict;*.jpg;*.jpeg;*.iff;*.hdr;*.gif;*.exr;*.bmp";
+            if (fd.ShowDialog() == true) { ThumbFilePath = fd.FileName; ThumbFilePathTBox.Text = fd.FileName; }
+        }
+
+        private void AudioRefButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "楽曲ファイル|*.mp3;*.ogg;*.wav;*.aif";
+            if (fd.ShowDialog() == true) { AudioFilePath = fd.FileName; AudioFilePathTBox.Text = fd.FileName; }
+        }
+
+        private void ClearButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            WorkSpaceDirectoryNameTBox.Text = "";
+            SongNameTBox.Text = "";
+            ArtistNameTBox.Text = "";
+            ThumbFilePathTBox.Text = "";
+            AudioFilePathTBox.Text = "";
+            ChartDesignerNameTBox.Text = "";
+            ChartDifficulitiesTBox.Text = "";
+            ChartLevelsTBox.Text = "";
+            StandardBPMTBox.Text = "";
+            WSDNErrorLabel.Content = "";
+            AFPErrorLabel.Content = "";
+            WorkSpaceDirectoryName = "";
+            SongName = "";
+            ArtistName = "";
+            ThumbFilePath = "";
+            AudioFilePath = "";
+            ChartDesignerName = "";
+            ChartDifficulities = "";
+            ChartLevels = "";
+            StandardBPM = "";
+        }
+
+        private void WorkSpaceDirectoryNameTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            if (WorkSpaceDirectoryNameTBox.Text.ToString() == "" || Directory.Exists(WSP + WorkSpaceDirectoryNameTBox.Text.ToString())) { WSDNErrorLabel.Content = "そのフォルダ名は使用できません!"; WorkSpaceDirectoryName = ""; }
+            else { WorkSpaceDirectoryName = WorkSpaceDirectoryNameTBox.Text.ToString(); WSDNErrorLabel.Content = ""; }
+        }
+
+        private void SongNameTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            SongName = SongNameTBox.Text.ToString();
+        }
+
+        private void ArtistNameTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ArtistName = ArtistNameTBox.Text.ToString();
+        }
+
+        private void ThumbFilePathTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            if (!File.Exists(ThumbFilePathTBox.Text.ToString())) { TFPErrorLabel.Content = "ファイルが存在しません!(「None」と入力すると使用しません)"; ThumbFilePath = ""; }
+            else { TFPErrorLabel.Content = ""; ThumbFilePath = ThumbFilePathTBox.Text.ToString(); }
+        }
+
+        private void AudioFilePathTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            if (!File.Exists(AudioFilePathTBox.Text.ToString())) { AFPErrorLabel.Content = "ファイルが存在しません!"; AudioFilePath = ""; }
+            else { AFPErrorLabel.Content = ""; AudioFilePath = AudioFilePathTBox.Text.ToString(); }
+        }
+
+        private void ChartDesignerNameTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ChartDesignerName = ChartDesignerNameTBox.Text.ToString();
+        }
+
+        private void ChartDifficulitiesTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ChartDifficulities = ChartDifficulitiesTBox.Text.ToString();
+        }
+
+        private void ChartJudgesTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ChartJudges = ChartJudgesTBox.Text.ToString();
+        }
+
+        private void ChartLevelsTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            ChartLevels = ChartLevelsTBox.Text.ToString();
+        }
+
+        private void StandardBPMTBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            StandardBPM = StandardBPMTBox.Text.ToString();
+        }
+    }
+}
